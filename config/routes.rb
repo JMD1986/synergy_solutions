ef6399c94d0e8284ca6e1907b019c3a2558b8a78
John Daugherty <johndaugherty1986@gmail.com>
2015-06-24 18:43:08 -0500

    course show page now includes all employees involved.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,42 @@ Rails.application.routes.draw do
   resources :courses
   resources :locations
   resources :companies
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+
+#   employees GET    /employees(.:format)          employees#index
+#               POST   /employees(.:format)          employees#create
+#  new_employee GET    /employees/new(.:format)      employees#new
+# edit_employee GET    /employees/:id/edit(.:format) employees#edit
+#      employee GET    /employees/:id(.:format)      employees#show
+#               PATCH  /employees/:id(.:format)      employees#update
+#               PUT    /employees/:id(.:format)      employees#update
+#               DELETE /employees/:id(.:format)      employees#destroy
+#       courses GET    /courses(.:format)            courses#index
+#               POST   /courses(.:format)            courses#create
+#    new_course GET    /courses/new(.:format)        courses#new
+#   edit_course GET    /courses/:id/edit(.:format)   courses#edit
+#        course GET    /courses/:id(.:format)        courses#show
+#               PATCH  /courses/:id(.:format)        courses#update
+#               PUT    /courses/:id(.:format)        courses#update
+#               DELETE /courses/:id(.:format)        courses#destroy
+#     locations GET    /locations(.:format)          locations#index
+#               POST   /locations(.:format)          locations#create
+#  new_location GET    /locations/new(.:format)      locations#new
+# edit_location GET    /locations/:id/edit(.:format) locations#edit
+#      location GET    /locations/:id(.:format)      locations#show
+#               PATCH  /locations/:id(.:format)      locations#update
+#               PUT    /locations/:id(.:format)      locations#update
+#               DELETE /locations/:id(.:format)      locations#destroy
+#     companies GET    /companies(.:format)          companies#index
+#               POST   /companies(.:format)          companies#create
+#   new_company GET    /companies/new(.:format)      companies#new
+#  edit_company GET    /companies/:id/edit(.:format) companies#edit
+#       company GET    /companies/:id(.:format)      companies#show
+#               PATCH  /companies/:id(.:format)      companies#update
+#               PUT    /companies/:id(.:format)      companies#update
+#               DELETE /companies/:id(.:format)      companies#destroy
+
+#   # The priority is based upon order of creation: first created -> highest priority.
+#   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
