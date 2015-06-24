de8a2c22eba4d947dc51a3ce6df26abce82b7d6d
John Daugherty <johndaugherty1986@gmail.com>
2015-06-24 17:32:26 -0500

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  company = Company.create(name: Faker::Company.name)
+  5.times do
+    10.times do
+      company.employees.create(name: Faker::Name.name,
+                              job_title: Faker::Name.title)
+  end
+    location = company.locations.create(name: Faker::Address.city)
+    3.times do
+      location.courses.create(title: Faker::Company.catch_phrase,
+                        description: Faker::Lorem.paragraph)
+    end
+  end
+end
